b62418417ff811ba5ed6bb8b05fc210040661397
Guest <guest@guests-mbp.home>
2019-07-16 19:20:26 +0200

    MESSAGE

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-This script is for matching certificates. 
+#Authors
 
+* Aly Abdellatif
+* Michel Estaben
+* Aladdin Chettouh
+* Koceila Chikhdene
+* Lounis Berrabah
+* Semi Dhouib
+
+
+#Keynder
+
+Keynder is a python program for matching public and private keys(Certificates,ASN.1,PEM,X509) in a given Folder(example : '~/Documents/'). Your Folder can contain any type of files(.bin,.zip,.txt,etc...)
+
+
+
+## Usage
 -h or --help:
 	This options prints the help file.
 
@@ -15,10 +30,16 @@ This script is for matching certificates.
 -m or --match:
 	This option is to be executed with or without the injection option. In case it is executed without injection, it matches the data already existing in the database.
 
-Examples:
-	python3 keynder.py -d '/home/project/directory/' -i -b 'mydatabase.db' -m (for the whole procedure, grabbing, injecting and matching);
+## Examples
 
-	python3 keynder.py -d '/home/project/directory/' -i -b 'mydatabase.db' (for grabbing and injecting without matching into exisiting database or database with custom name);
-	python3 keynder.py -d '/home/project/directory/' -b 'mydatabase.db' -m (matches data into the already existing database);
+```python
 
-	python3 keynder.py -d '/home/project/directory/' -i -m (injects into the defaults database 'certs.db' and matches data. If the database does not exist it will create it).
+	python3 main.py -d '/home/project/directory/' -i -b 'mydatabase.db' -m #(for the whole procedure, grabbing, injecting and matching);
+
+	python3 main.py -d '/home/project/directory/' -i -b 'mydatabase.db' #(for grabbing and injecting without matching into exisiting database or database with custom name);
+	python3 main.py -d '/home/project/directory/' -b 'mydatabase.db' -m #(matches data into the already existing database);
+
+	python3 main.py -d '/home/project/directory/' -i -m #(injects into the defaults database 'certs.db' and matches data. If the database does not exist it will create it).
+
+## License
+[GNU GENERAL PUBLIC LICENSE V3] (https://www.gnu.org/licenses/gpl-3.0.en.html)
---
+++ src/Classifier.py
@@ -0,0 +1,100 @@
+
+from Data import Data
+from Key import Key
+from Certificate import Certificate
+from StringGrabber import StringGrabber as Grabber
+from Exceptions import *
+
+class Classifier:
+
+    def __init__(self, grabber):
+        self.data_list = grabber.get_data()
+        self.objects_list = []
+        self.keys_list = None
+        self.certs_list = None
+        
+    def detect_type(self):
+        
+        for data in self.data_list:
+            d = Data(data)
+            if(d.my_type=='ENC'):
+                continue
+            elif('KEY' in d.my_type):
+                d = Key(data) #creation de la class Key            
+            elif('CRT' in d.my_type):
+                try:
+                    d = Certificate(data, 'PEM') #creation de la class Certificat avec PEM
+                except:
+                    continue
+            
+            elif('DER' in d.my_type):
+                try:
+                    d = Certificate(data, 'DER') #creation de la class Certificat avec DER
+                except:
+                    continue
+            self.objects_list.append(d)
+        return len(self.objects_list)
+
+    def remove_duplicate(self):
+        liste_keys = []
+        liste_certifs = []
+        # Trie du tableau de base pour créer une liste de cetificats et une liste de clés
+        for objet in self.objects_list :
+            if type(objet) is Key : 
+                liste_keys.append(objet)
+            elif type(objet) is Certificate :
+                liste_certifs.append(objet)
+            else :
+                continue
+        #_______________________________________________________________
+
+
+        # On supprime les doublons de clés
+        #_______________________________________________________________
+        for k1 in liste_keys :
+            index = liste_keys.index(k1)
+            for k2 in liste_keys[index+1:] :
+                if k1.data == k2.data :
+                    index2 = liste_keys.index(k2)
+                    del liste_keys[index2]
+        #_______________________________________________________________
+
+
+        # On supprime les doublons dans les certificats
+        #_______________________________________________________________
+        for c1 in liste_certifs :
+            index = liste_certifs.index(c1)
+            for c2 in liste_certifs[index+1:] :
+                if c1.pub_key == c2.pub_key :
+                    index2 = liste_certifs.index(c2)
+                    del liste_certifs[index2]
+
+        #_______________________________________________________________
+
+
+        # On supprime les clés publiques des certificats
+        #_______________________________________________________________
+        for k in liste_keys :
+            index = liste_keys.index(k)
+            for c in liste_certifs :
+                if str(k.data).rstrip("\n\r") == str(c.pub_key.rstrip("\n\r")) :
+                    del liste_keys[index]
+
+        #return liste_keys,liste_certifs
+        self.keys_list = liste_keys
+        self.certs_list = liste_certifs
+
+    def classify(self):
+        self.detect_type()
+        self.remove_duplicate()
+
+    def get_data(self):
+        return self.certs_list,self.keys_list
+
+### Main ###
+# directory = '/mnt/hgfs/Home/Google\ Drive/M2/projet-GN/data/'
+# grabber = Grabber(directory)
+# classifier = Classifier(grabber)
+# classifier.classify()
+#print(classifier.objects_list)
+
